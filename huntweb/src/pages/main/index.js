aa644ad3a2af374f9c2f11fb4edb2abf0efa6d20
wenzler15 <douglas_wenzler@hotmail.com>
2020-09-20 18:02:53 -0300

    Finished project

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 export default class Main extends Component {
@@ -54,7 +55,7 @@ export default class Main extends Component {
           <article key="product._id">
             <strong>{product.title}</strong>
             <p> {product.description} </p>
-            <a href="">Acessar</a>
+            <Link to={`/products/${product._id}`}>Acessar</Link>
           </article>
         ))}
         <div className="actions">
